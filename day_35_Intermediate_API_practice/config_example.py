6452c997d92bbceabb339b141b11bd95fbea0172
Rafa Plamadeala <raf.plamadeala@gmail.com>
2026-02-09 14:35:34 +0000

    Remove hardcoded Twilio/OpenWeather credentials from day 35

--- day_35_Intermediate_API_practice/config_example.py
+++ day_35_Intermediate_API_practice/config_example.py
@@ -1,3 +1,5 @@
 sms_account_sid = "ACxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 auth_token = "PUT_YOUR_TWILIO_AUTH_TOKEN"
 weather_key = "PUT_YOUR_OPENWEATHER_KEY"
+from_number = "PUT FROM NUMBER HERE"
+to_number = "PUT TO NUMBER HERE"
--- day_35_Intermediate_API_practice/main.py
+++ day_35_Intermediate_API_practice/main.py
@@ -1,48 +1,46 @@
+import os
 import requests
 from twilio.rest import Client
-import config
+import config_example
 
 # http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
 # https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
 
-sms_account_sid = config.sms_account_sid
-auth_token = config.auth_token
-weather_key = config.weather_key
+sms_account_sid = config_example.sms_account_sid
+auth_token = config_example.auth_token
+weather_key = config_example.weather_key
 
 # Extracting Location from Geocoding API.
-url_geo = "http://api.openweathermap.org/geo/1.0/direct?q=London,GB&limit=3&appid=ae5510839ccbc5651b43654e06572fe2"
-r =  requests.get(url_geo, timeout=10)
+url_geo = "http://api.openweathermap.org/geo/1.0/direct"
+geo_params = {"q": "London,GB", "limit": 3, "appid": weather_key}
+
+r = requests.get(url_geo, params=geo_params, timeout=10)
 r.raise_for_status()
 data = r.json()
+
 loc = data[0]
-lat, lon = 39.601215, -9.070099
+lat, lon = loc["lat"], loc["lon"]
 
-params = {
-    "lat": lat,
-    "lon": lon,
-    "appid": weather_key,
-    "cnt": 4,
-}
+params = {"lat": lat, "lon": lon, "appid": weather_key, "cnt": 4}
 
-# Extracting Weather with previous codes.
+# Extracting Weather forecast
 url_weather = "https://api.openweathermap.org/data/2.5/forecast"
-r =  requests.get(url_weather, params=params, timeout=10)
+r = requests.get(url_weather, params=params, timeout=10)
 r.raise_for_status()
 data = r.json()
 
 def check_rain():
     for entry in data.get("list", []):
-        weather = entry["weather"]
-        for element in weather:
-            if element.get("id") < 700:
+        for element in entry.get("weather", []):
+            wid = element.get("id")
+            if wid is not None and wid < 700:
                 client = Client(sms_account_sid, auth_token)
-                message = client.messages.create(
+                msg = client.messages.create(
                     body="It is going to rain. Remember to bring an ☂️",
-                    from_="+15075194228",
-                    to="+351937048312",
+                    from_=config_example.from_number,
+                    to=config_example.to_number,
                 )
-                print(message.sid)
-                print(message.status)
+                print(msg.sid, msg.status)
                 return
-    
+
 check_rain()
